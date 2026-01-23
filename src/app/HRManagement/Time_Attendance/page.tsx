fa203696674a23b807795d73daeea6aa59ac75d4
Arcarlear <ocfitasarcarlear@gmail.com>
2026-01-23 20:48:59 +0700

    Update page.tsx

--- src/app/HRManagement/Time_Attendance/page.tsx
+++ src/app/HRManagement/Time_Attendance/page.tsx
@@ -2,12 +2,19 @@
 
 import React, { useState } from "react"
 import Sidebar from "@/app/components/Sidebar"
+import { Mail, Phone, Building2, Calendar, Pencil } from "lucide-react"
+import Image from 'next/image'
 
 export default function Time_Attendance() {
     return (
         <div className="flex bg-white font-[Prompt] min-h-screen text-black">
             <Sidebar />
+
             <div className="flex flex-col m-[3%] w-3/4">
+                {/* โปรไฟล์พนักงาน */}
+                <h1 className="text-3xl font-bold text-[#DF5E10] mb-10">
+                    โปรไฟล์พนักงาน
+                </h1>
             </div>
         </div>
     )
