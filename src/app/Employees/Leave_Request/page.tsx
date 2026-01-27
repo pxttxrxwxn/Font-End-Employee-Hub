2b1a2887951501b622238d860392795b4be16629
pxttxrxwxn <naysasatadur5555@gmail.com>
2026-01-28 01:50:40 +0700

    Add Leave_Request component with SidebarEmployees integration

---
+++ src/app/Employees/Leave_Request/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from "react"
+import Sidebar from "@/app/components/SidebarEmployees"
+
+export default function Leave_Request() {
+    return (
+        <div className="flex bg-white font-[Prompt] min-h-screen text-black">
+            <Sidebar />
+            <div className="flex flex-col m-[3%] w-3/4">
+            </div>
+        </div>
+    )
+}
