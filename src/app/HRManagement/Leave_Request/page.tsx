0f3beaa442d38306024c4c037885e00d21458147
pxttxrxwxn <naysasatadur5555@gmail.com>
2026-01-28 02:00:34 +0700

    Update Leave_Request component title and add submission button

--- src/app/HRManagement/Leave_Request/page.tsx
+++ src/app/HRManagement/Leave_Request/page.tsx
@@ -11,10 +11,20 @@ export default function Leave_Request() {
             <div className="flex flex-col m-[3%] w-3/4">
                 <div className="flex w-full items-start justify-between">
                     <h1 className="text-3xl font-bold text-[#DF5E10] mb-10">
-                        จัดการแผนกและตำแหน่ง
+                        คำร้องขอลา
                     </h1>
                     <Bell size={30} />
-                    
+                </div>
+                <div className="flex w-full items-center justify-end mb-6">
+                    <div
+                        className="flex bg-[#134BA1] text-white px-4 py-2 rounded-xl text-xl items-center gap-1 cursor-pointer hover:bg-[#0f3a80] transition-colors"
+                    >
+                        <Plus />
+                        ยื่นคำร้องขอลา
+                    </div>
+                </div>
+                <div className="flex w-full items-end justify-start mb-6 text-2xl font-bold">
+                    <FileText size={30} />รายการยื่นคำร้องขอลา
                 </div>
             </div>
         </div>
