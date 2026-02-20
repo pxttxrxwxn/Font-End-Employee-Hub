2a9817b82176e50dd5aac2f449611429b56cd1fb
pxttxrxwxn <naysasatadur5555@gmail.com>
2026-02-20 11:52:39 +0700

    Update api.ts

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -1,4 +1,4 @@
-// const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8787';
+// export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const API_URL = 'http://localhost:8787';
 
 const getToken = () => {
